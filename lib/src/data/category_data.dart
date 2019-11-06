3bf31c5568a5b3e591d622049f8a21fa9ff5417f
DIVYANSHU SINGH <divyanshusingh7nov@gmail.com>
2019-11-06 20:34:41 +0530

    Add files via upload

---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,21 @@
+import '../models/category_model.dart';
+
+final categories = [
+  Category(
+    numberOfItems: 4,
+    imagePath: "assets/images/burger.png",
+    categoryName: "Burger"
+  ),
+
+  Category(
+    numberOfItems: 2,
+    imagePath: "assets/images/pizza.png",
+    categoryName: "Pizza"
+  ),
+
+    Category(
+    numberOfItems: 5,
+    imagePath: "assets/images/coffee-cup.png",
+    categoryName: "Coffe Cup"
+  ),
+];
---
+++ lib/src/data/food_data.dart
@@ -0,0 +1,36 @@
+import '../models/food_model.dart';
+
+final foods = [
+  Food(
+    id: "1",
+    name: "Maggi Hotspot",
+    imagePath: "assets/images/hotspot.jpg",
+    category: "1",
+    discount: 33.5,
+    ratings: 99.0,
+  ),
+  Food(
+    id: "2",
+    name: "Kathi Junction",
+    imagePath: "assets/images/kathi.jpg",
+    category: "2",
+    discount: 34.5,
+    ratings: 69.0,
+  ),
+  Food(
+    id: "3",
+    name: "Quench",
+    imagePath: "assets/images/quench.jpg",
+    category: "3",
+    discount: 32.5,
+    ratings: 75.0,
+  ),
+  Food(
+    id: "4",
+    name: "Surya Snacks Corner",
+    imagePath: "assets/images/Surya.jpg",
+    category: "4",
+    discount: 30.5,
+    ratings: 98.0,
+  ),
+];
